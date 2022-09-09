8a0b40ebb3c4807b6c16bc71a853c0a86eb5eaed
databoose <K1NGKAOS100@Gmail.com>
2022-09-09 08:52:51 -0700

    first commit

---
+++ pynote.py
@@ -0,0 +1,23 @@
+import os
+import readline
+
+def writing():
+    msg = input("Enter your message\n\n>")
+
+
+if __name__ == "__main__":
+    print("----------------------------------")
+    print("-PyNote, a python journal logger-")
+    print("----------------------------------")
+    if os.access("journal.txt", os.R_OK) is True:
+        writing()
+
+    elif os.access("journal.txt", os.R_OK) is False:
+        print("journal.txt will be created in current directory as it does not exist.\n")
+        answer = input("Is this okay? (Enter Y/N) : ")
+        if answer == "Y":
+            fp = open('journal.txt', 'w')
+            print("journal file has been created\n")
+            writing()
+        elif answer == "N":
+            quit()
